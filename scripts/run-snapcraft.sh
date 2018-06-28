9e4676ccf76883a6661342e65c8789d3feb3b1aa
Oliver Kopp <kopp.dev@gmail.com>
2018-06-28 10:32:34 +0200

    Switch to latest snapcraft image

--- scripts/run-snapcraft.sh
+++ scripts/run-snapcraft.sh
@@ -3,4 +3,4 @@ VERSION=`grep "^version =" build.gradle | sed "s/^version = \"\(.*\)\".*/\1/"`
 sed -i "s/4.0-dev/$VERSION/" snapcraft.yaml
 
 # last parameter: edge | beta | stable
-docker run -v $(pwd):$(pwd) -t koppor/snapcraft-java-de:v1.4.0 sh -c "cd $(pwd) && snapcraft && snapcraft push *.snap --release edge"
+docker run -v $(pwd):$(pwd) -t koppor/snapcraft-java-de:latest sh -c "cd $(pwd) && snapcraft && snapcraft push *.snap --release edge"
